827e8e9b9aabdae54d3e0477845e0356f454623d
ProgrammerIosif <programmeriosif@protonmail.com>
2024-09-28 16:55:38 +0300

    added tailwind as dependency and not from cdn

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="m-0 p-0">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
 
         <title>Laravel</title>
 
@@ -10,10 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <!-- Install Tailwind -->
-        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>
-      {{ $slot }}
+        {{ $slot }}
     </body>
 </html>
