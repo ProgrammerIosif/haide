d465bc9a55a97c4aeab03235a645069c61f89491
ProgrammerIosif <programmeriosif@protonmail.com>
2024-10-10 14:35:56 +0300

    form fields will not reset on validation failure

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,8 +5,8 @@
             <div>
                 <p class="mt-1 text-sm leading-6">We just need a handful of details from you.</p>
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <x-form-field name="name" required>Name</x-form-field>
-                    <x-form-field type="email" name="email" required>Email</x-form-field>
+                    <x-form-field name="name" :value="old('name')" required>Name</x-form-field>
+                    <x-form-field type="email" name="email" :value="old('email')" required>Email</x-form-field>
                     <x-form-field type="password" name="password" required>Password</x-form-field>
                     <x-form-field type="password" name="password_confirmation" required>Confirm password</x-form-field>
                 </div>
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -6,8 +6,8 @@
                 <h2 class="text-base font-semibold leading-7">Create a New Event</h2>
                 <p class="mt-1 text-sm leading-6">We just need a handful of details from you.</p>
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <x-form-field name="name" required>Name</x-form-field>
-                    <x-form-field name="location" required>Location</x-form-field>
+                    <x-form-field name="name" :value="old('name')" required>Name</x-form-field>
+                    <x-form-field name="location" :value="old('location')" required>Location</x-form-field>
                 </div>
             </div>
             <x-anchor tag="button" type="secondary" size="small">Create</x-anchor>
